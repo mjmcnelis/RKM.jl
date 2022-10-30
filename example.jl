3e1073c5d9ca6cb1541b660366244a32b6a2dfa2
Michael <Michael@Michael-McNeliss-MacBook-Pro.local>
2022-10-30 00:38:56 -0600

    made progress on allocation, about 50x less than old julia version

--- example.jl
+++ example.jl
@@ -1,24 +1,34 @@
 
 using Revise
 using RKM
+using LinearAlgebra
+using Plots 
+plotly()
 
-# TODO: better interface? (MTK?)
-function y_prime(t, y) 
-    (y + 0.5) * (0.5 - y)
-end
+# # # TODO: better interface? (MTK?)
+# function dy_dt!(f, t, y; A = 0.5)      # need to pass in a struct somewhere 
+#     # f .= (y .+ A) .* (A .- y)
+#     f[1] = (y[1] + A) * (1.0 - A - y[1])
+#     nothing
+# end
 
-# should handle scalar and vector cases? or just vector
-y0 = -1.0
-t_span = TimeSpan(; t0 = -10.0, tf = 10.0, dt0 = 2.0)
+t0 = -10.0
+y0 = exp(t0) / (1.0 + exp(t0)) - 0.5
+
+t_span = TimeSpan(; t0, tf = 10.0, dt0 = 0.001)
 
 adaptive   = Fixed()
 method     = Heun2()
 parameters = Parameters(; adaptive, method)
 
+@time sol = evolve_ode(y0, t_span, dy_dt!; parameters)
 
-@time sol = evolve_ode(y0, t_span, y_prime; parameters)
-
-# @show sol.t sol.dt
+# y = reduce(hcat, sol.y)'
+# t = sol.t 
+# plot(t, y; size = (900, 600), linewidth = 2,
+#            legend = :outertopright, legendfontsize = 14,
+#            ylabel = "y", yguidefontsize = 14, ytickfontsize = 12,
+#            xlabel = "t", xguidefontsize = 14, xtickfontsize = 12,) |> display
 
 println("\ndone")
 
--- src/evolve.jl
+++ src/evolve.jl
@@ -1,10 +1,45 @@
 
-function evolve_ode(y0::Union{AbstractFloat, Vector{<:AbstractFloat}}, t_span, y_prime; 
-                    parameters::Parameters)
+function evolve_one_time_step!(method::RungeKutta, adaptive::Fixed,
+                               y::Vector{<:AbstractFloat}, t::AbstractFloat, 
+                               dt::AbstractFloat, dy_dt!::Function)
 
-    @unpack adaptive, method = parameters
+    butcher = method.butcher            # non-allocating
+   
+    stages = size(butcher, 1) - 1
+    dimensions = size(y, 1)
+
+    dy = zeros(stages, dimensions)      # TODO: preallocate
+    y_tmp = zeros(dimensions)        
+    f_tmp = zeros(dimensions) 
+
+    for i = 1:stages                    # note: assumes explicit
+        c = butcher[i, 1]
+        t_tmp = t + c*dt
+        y_tmp .= y
+        for j = 1:i-1
+            A = butcher[i, j+1]
+            y_tmp .+= A * dy[j]      # I wonder if I preallocate dy, allocations are gone
+        end
+        dy_dt!(f_tmp, t_tmp, y_tmp)
+
+        dy[i, :] .= dt .* f_tmp
+    end    
+
+    for j = 1:stages 
+        b = butcher[end, j+1]
+        y .+= b * dy[j]
+    end
+
+    nothing
+end
+
+function evolve_ode(y0::Union{AbstractFloat, Vector{<:AbstractFloat}}, 
+                    t_span::TimeSpan, dy_dt!::Function; 
+                    parameters::Parameters, wtime_min = 60)
+
     @unpack t0, tf, dt0 = t_span
-
+    @unpack adaptive, method = parameters
+    
     precision = get_precision(method)
 
     # initial conditions
@@ -13,28 +48,27 @@ function evolve_ode(y0::Union{AbstractFloat, Vector{<:AbstractFloat}}, t_span, y
     dt = dt0 |> precision
 
     # initialize arrays 
-    y_array  = Vector{Vector{precision}}()
-    t_array  = Vector{precision}()
-    dt_array = Vector{precision}()
+    y_arr  = Vector{Vector{precision}}()
+    t_arr  = Vector{precision}()
+    dt_arr = Vector{precision}()
 
-    time_limit = Dates.now() + Dates.Minute(1)
+    time_limit = Dates.now() + Dates.Minute(round(wtime_min))
 
     # TODO: wrap inside function 
     while true
-        push!(y_array, y)
-        push!(t_array, t)
-
-        # TODO fill out step 
-        # y += 1
+        push!(y_arr, copy(y)) 
+        push!(t_arr, t)
+       
+        evolve_one_time_step!(method, adaptive, y, t, dt, dy_dt!)
 
         time_now = Dates.now()
 
         # TODO: throw LongSolve exception
         (t < tf && time_now < time_limit) || break
 
-        push!(dt_array, dt)
+        push!(dt_arr, dt)
         t += dt
     end
     
-    Solution(; y = y_array, t = t_array, dt = dt_array)
+    Solution(; y = y_arr, t = t_arr, dt = dt_arr)
 end
