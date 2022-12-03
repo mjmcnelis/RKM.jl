c71517de436aef70e49c6bcb20c8dac34860bea1
Michael McNelis <mjmcnelis@gmail.com>
2022-12-03 14:10:35 -0700

    adjusted parameters in linear advection for journal club, also output to png

--- demo/linear_advection/backward/central.jl
+++ demo/linear_advection/backward/central.jl
@@ -2,19 +2,19 @@
 using Revise
 using RKM
 using LinearAlgebra
-using Plots 
+using Plots
 using UnPack
-plotly()
+# plotly()
 
 function gauss(x)
-    exp(-(x - 2)^2)
+    exp(-(x - 3)^2)
 end
 
-# initial condition 
-x = LinRange(0, 10, 101) |> collect
+# initial condition
+x = LinRange(0, 12, 121) |> collect
 const a = 1.0
 const dx = x[2] - x[1]
-const dt = 0.05           # just adjust this for courant number 
+const dt = 0.05           # just adjust this for courant number
 const C = a*dt/dx
 N = 40
 
@@ -25,6 +25,8 @@ function dy_dt!(f, t, y)
     for i in 1:L
         m = max(i-1, 1) # BC: y[0] = y[1]
         p = min(i+1, L) # BC: y[L+1] = y[L]
+        # i == 1 ? m = L : nothing
+        # i == L ? p = 1 : nothing
         ym, yp = y[m], y[p]
 
         f[i] = -(F(yp) - F(ym))/(2.0*dx)
@@ -33,9 +35,9 @@ function dy_dt!(f, t, y)
 end
 
 function jacobian!(J, t, y)
-    nrow = size(J,1) 
+    nrow = size(J,1)
     A = a/(2.0*dx)
-    # note: includes NBC 
+    # note: includes NBC
     J[1,1] = A
     J[1,2] = -A
     J[end,end] = -A
@@ -44,7 +46,7 @@ function jacobian!(J, t, y)
         J[i,i-1] = A
         J[i,i+1] = -A
     end
-    nothing 
+    nothing
 end
 
 adaptive   = Fixed()
@@ -54,28 +56,28 @@ parameters = Parameters(; adaptive, method, t_span)
 
 @unpack t0, dt0 = t_span
 y0 = gauss.(x)
-@show C 
+@show C
 
 @time sol = evolve_ode(y0, dy_dt!; jacobian!, parameters)
 
 plt = plot(x, y0, label = "t = 0", color = "indianred", linewidth = 2,
            size = (900, 600), ylims = (-0.5, 1.3),
-           ylabel = "u", yguidefontsize = 14, ytickfontsize = 12, 
-           xlabel = "x", xguidefontsize = 14, xtickfontsize = 12, 
-           legend = :outertopright, legendfontsize = 12)
+           ylabel = "u", yguidefontsize = 14, ytickfontsize = 12,
+           xlabel = "x", xguidefontsize = 14, xtickfontsize = 12,
+           legend = :outertopright, legendfontsize = 12, dpi = 200)
 for i = 1:3
-    t = t0 + N*i*dt0 
+    t = t0 + N*i*dt0
     y = sol.y[1 + N*i]
     plot!(x, y, color = "indianred", linewidth = 2, label = "t = $t")
 end
 
-plot!(x, y0, label = "t = 0 (exact)", color = "black", linewidth = 1, line = :dash)   
+plot!(x, y0, label = "t = 0 (exact)", color = "black", linewidth = 1, line = :dash)
 for i = 1:3
-    t = t0 + N*i*dt0 
+    t = t0 + N*i*dt0
     y_exact = gauss.(x .- a*t)
     plot!(x, y_exact, color = "black", linewidth = 1, line = :dash, label = "t = $t (exact)")
 end
-display(plt)
+# display(plt)
+savefig(plt, joinpath(RKM_root, "demo/linear_advection/plot_central.png"))
 
 println("\ndone")
-
--- demo/linear_advection/backward/lax_friedrichs.jl
+++ demo/linear_advection/backward/lax_friedrichs.jl
@@ -2,27 +2,28 @@
 using Revise
 using RKM
 using LinearAlgebra
-using Plots 
+using Plots
 using UnPack
-plotly()
+# plotly()
 
 function gauss(x)
-    exp(-(x - 2)^2)
+    exp(-(x - 3)^2)
 end
 
-# initial condition 
-x = LinRange(0, 10, 101) |> collect
+# initial condition
+x = LinRange(0, 12, 121) |> collect
 const a = 1.0
 const dx = x[2] - x[1]
-const dt = 0.05           # just adjust this for courant number 
+const dt = 0.05           # just adjust this for courant number
 const C = a*dt/dx
 N = 40
+
 
 F(y) = a*y
 
 # TODO: try newton method to fix instability of backward LxF
 function dy_dt!(f, t, y)
-    L = length(y) 
+    L = length(y)
     for i in 1:L
         m = max(i-1, 1) # BC: y[0] = y[1]
         p = min(i+1, L) # BC: y[L+1] = y[L]
@@ -34,20 +35,20 @@ function dy_dt!(f, t, y)
 end
 
 function jacobian!(J, t, y)
-    nrow = size(J,1) 
+    nrow = size(J,1)
     A = a/(2.0*dx)
     B = 1.0/(2.0*dt)
-    # note: includes NBC 
+    # note: includes NBC
     J[1,1]       = A - B
-    J[1,2]       = B - A 
-    J[end,end]   = -A - B 
+    J[1,2]       = B - A
+    J[end,end]   = -A - B
     J[end,end-1] = A + B
     for i in 2:nrow-1
         J[i,i-1] = A + B
         J[i,i]   = -2.0*B
         J[i,i+1] = B - A
     end
-    nothing 
+    nothing
 end
 
 adaptive   = Fixed()
@@ -57,28 +58,28 @@ parameters = Parameters(; adaptive, method, t_span)
 
 @unpack t0, dt0 = t_span
 y0 = gauss.(x)
-@show C 
+@show C
 
 @time sol = evolve_ode(y0, dy_dt!; jacobian!, parameters)
 
 plt = plot(x, y0, label = "t = 0", color = "indianred", linewidth = 2,
            size = (900, 600), ylims = (-0.5, 1.3),
-           ylabel = "u", yguidefontsize = 14, ytickfontsize = 12, 
-           xlabel = "x", xguidefontsize = 14, xtickfontsize = 12, 
-           legend = :outertopright, legendfontsize = 12)
+           ylabel = "u", yguidefontsize = 14, ytickfontsize = 12,
+           xlabel = "x", xguidefontsize = 14, xtickfontsize = 12,
+           legend = :outertopright, legendfontsize = 12, dpi = 200)
 for i = 1:3
-    t = t0 + N*i*dt0 
+    t = t0 + N*i*dt0
     y = sol.y[1 + N*i]
     plot!(x, y, color = "indianred", linewidth = 2, label = "t = $t")
 end
 
-plot!(x, y0, label = "t = 0 (exact)", color = "black", linewidth = 1, line = :dash)   
+plot!(x, y0, label = "t = 0 (exact)", color = "black", linewidth = 1, line = :dash)
 for i = 1:3
-    t = t0 + N*i*dt0 
+    t = t0 + N*i*dt0
     y_exact = gauss.(x .- a*t)
     plot!(x, y_exact, color = "black", linewidth = 1, line = :dash, label = "t = $t (exact)")
 end
-display(plt)
+# display(plt)
+savefig(plt, joinpath(RKM_root, "demo/linear_advection/plot_lax_friedrichs.png"))
 
 println("\ndone")
-
