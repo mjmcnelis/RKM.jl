8c06b9e40529c386cf30397f0f7c2c64e842f247
Michael <Michael@Michael-McNeliss-MacBook-Pro.local>
2023-05-26 12:55:31 -0400

    combined explicit/implicit steps, done reorganizing runge_kutta/ folder for now

--- src/updates/runge_kutta/fixed_step.jl
+++ src/updates/runge_kutta/fixed_step.jl
@@ -1,31 +1,26 @@
 
-function evolve_one_time_step!(method::RungeKutta, iteration::Explicit,
-             adaptive::Fixed, ::Controller, FE::MVector{1,Int64}, 
-             y::VectorMVector, t::VectorMVector{1,T}, dt::VectorMVector{2,T}, 
-             dy_dt!::F, dy::MatrixMMatrix, y_tmp::VectorMVector, 
-             f_tmp::VectorMVector, args...) where {T <: AbstractFloat, F}
-          
-    # note: I can comment this out and loop i = 1:stages w/o allocating
-    dy_dt!(f_tmp, t[1], y)                              # evaluate first stage at (t,y)
-    @.. dy[:,1] = dt[1] * f_tmp
-
-    runge_kutta_step!(method, iteration, y, t[1], dt[1], dy_dt!, dy, y_tmp, f_tmp)
-    @.. y = y_tmp                                       # get iteration
-
-    add_function_evaluations!(FE, iteration, adaptive, method)
-    return nothing
-end
-
-function evolve_one_time_step!(method::RungeKutta, iteration::DiagonalImplicit,
-             adaptive::Fixed, ::Controller, FE::MVector{1,Int64},
+function evolve_one_time_step!(method::RungeKutta, iteration::Iteration,
+             adaptive::Fixed, controller::Controller, FE::MVector{1,Int64},
              y::VectorMVector, t::VectorMVector{1,T}, dt::VectorMVector{2,T},
              dy_dt!::F, dy::MatrixMMatrix, y_tmp::VectorMVector, 
              f_tmp::VectorMVector, f::VectorMVector, y1, y2, error, 
              J::MatrixMMatrix, linear_cache, dy_dt_wrap!,
              stage_finder::ImplicitStageFinder) where {T <: AbstractFloat, F}
+    # note: for explicit, can comment this and loop i = 1:stages w/o allocating
+    if iteration isa Explicit
+        dy_dt!(f_tmp, t[1], y)                          # evaluate first stage at (t,y)
+        @.. dy[:,1] = dt[1] * f_tmp
+    end
 
     runge_kutta_step!(method, iteration, y, t[1], dt[1], dy_dt!, dy, y_tmp, 
                       f_tmp, J, linear_cache, dy_dt_wrap!, stage_finder)
     @.. y = y_tmp
+    
+    if iteration isa Explicit
+        add_function_evaluations!(FE, iteration, adaptive, method)
+    else 
+        # TMP 
+        FE[1] += 10
+    end
     return nothing
 end
