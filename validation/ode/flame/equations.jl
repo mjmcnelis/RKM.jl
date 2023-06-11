b0f7b57de4dc5de791ef4366a46249019fe044b1
Michael <Michael@Michael-McNeliss-MacBook-Pro.local>
2023-06-11 16:51:24 -0600

    added flame ode

---
+++ validation/ode/flame/equations.jl
@@ -0,0 +1,11 @@
+# for RKM
+function dy_dt!(f, t, y)
+    f[1] = y[1]^2*(1.0 - y[1]) 
+    return nothing 
+end
+
+# for OrdinaryDiffEq 
+function dy_dt!(f, y, p, t)
+    f[1] = y[1]^2*(1.0 - y[1]) 
+    return nothing 
+end
---
+++ validation/ode/flame/flame.jl
@@ -0,0 +1,36 @@
+using Revise, RKM, OrdinaryDiffEq
+using Plots; plotly()
+!(@isdefined dy_dt!) ? include("$RKM_root/validation/ode/flame/equations.jl") : nothing
+
+δ = 1e-4
+y0 = [δ]
+t0 = 0.0 
+tf = 2.0/δ
+dt0 = δ
+epsilon = 1e-7
+
+method = TrapezoidRuleBDF2()
+
+parameters = Parameters(; method, 
+                          adaptive = Doubling(; epsilon), 
+                          stage_finder = ImplicitStageFinder(; jacobian_method = ForwardJacobian()),
+                          t_range = TimeRange(; t0, tf, dt0)
+                        )
+@time sol = evolve_ode(y0, dy_dt!; parameters)
+get_stats(sol)
+plot_ode(sol, method, Plots.plot);
+
+prob = ODEProblem(dy_dt!, y0, (t0, tf))
+@time sol = solve(prob, TRBDF2(), dt = dt0, reltol = epsilon, abstol = epsilon)
+
+if sol.retcode == ReturnCode.Success
+    display(plot!(sol.t,  mapreduce(permutedims, vcat, sol.u), 
+                  color = :black, linewidth = 2, line = :dash)
+            )
+    @show sol.destats
+else 
+    @show sol.retcode
+end
+
+GC.gc()
+println("\ndone")
