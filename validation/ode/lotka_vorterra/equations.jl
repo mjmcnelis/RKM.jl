a961039263bc0efd15422fe2051d9e3e4bad1092
Michael <Michael@Michael-McNeliss-MacBook-Pro.local>
2023-06-24 17:12:47 -0600

    added lotka vorterra  example:

---
+++ validation/ode/lotka_vorterra/equations.jl
@@ -0,0 +1,15 @@
+# for RKM
+function dy_dt!(f, y; p, kwargs...)
+    α, β, γ, δ = p
+    f[1] = (α - β*y[2])*y[1]
+    f[2] = (δ*y[1] - γ)*y[2]
+    return nothing
+end
+
+# for OrdinaryDiffEq
+function dy_dt!(f, y, p, t)
+    α, β, γ, δ = p
+    f[1] = (α - β*y[2])*y[1]
+    f[2] = (δ*y[1] - γ)*y[2]
+    return nothing
+end
---
+++ validation/ode/lotka_vorterra/lotka_vorterra.jl
@@ -0,0 +1,31 @@
+using Revise, RKM, OrdinaryDiffEq, LinearSolve
+using Plots; plotly()
+!(@isdefined dy_dt!) ? include("$RKM_root/validation/ode/lotka_vorterra/equations.jl") : nothing
+
+y0 = [1.0, 1.0]
+t0 = 0.0
+tf = 10.0
+
+p = [1.5, 1.0, 3.0, 1.0]
+dt0 = 1e-4
+epsilon = 1e-8
+
+# RKM
+method = DormandPrince54()
+parameters = Parameters(; method,
+                          adaptive = Embedded(; epsilon),
+                          t_range = TimeRange(; t0, tf, dt0)
+                        )
+@time sol = evolve_ode(y0, dy_dt!; model_parameters = p, parameters, show_progress = false)
+get_stats(sol)
+plot_ode(sol, method, Plots.plot);
+
+# OrdinaryDiffEq
+prob = ODEProblem(dy_dt!, y0, (t0, tf), p)
+@time sol = solve(prob, DP5(), reltol = epsilon, abstol = epsilon, dt = dt0)
+@show sol.destats
+plot!(sol.t,  mapreduce(permutedims, vcat, sol.u),
+      color = :black, linewidth = 2, line = :dash) |> display
+
+GC.gc()
+println("\ndone")
