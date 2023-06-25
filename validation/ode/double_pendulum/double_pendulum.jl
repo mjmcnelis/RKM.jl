a2b320bf7517dd2f326c7ccfe7078f97f7983fe3
Michael <Michael@Michael-McNeliss-MacBook-Pro.local>
2023-06-24 21:36:11 -0600

    added double pendulum example (canonical eqs)

---
+++ validation/ode/double_pendulum/double_pendulum.jl
@@ -0,0 +1,32 @@
+using Revise, RKM, OrdinaryDiffEq, LinearSolve
+using Plots; plotly()
+!(@isdefined dy_dt!) ? include("$RKM_root/validation/ode/double_pendulum/equations.jl") : nothing
+
+y0 = [pi/2.0, pi/2.0, 0.0, 0.0]
+t0 = 0.0
+tf = 10.0
+
+p = [0.5]
+dt0 = 1e-4
+
+# RKM
+method = RungeKutta4()
+parameters = Parameters(; method,
+                          adaptive = Fixed(),
+                          t_range = TimeRange(; t0, tf, dt0)
+                        )
+@time sol = evolve_ode(y0, dy_dt!; model_parameters = p, parameters,
+                                   show_progress = false
+                      )
+get_stats(sol)
+plot_ode(sol, method, Plots.plot);
+
+# OrdinaryDiffEq
+prob = ODEProblem(dy_dt!, y0, (t0, tf), p)
+@time sol = solve(prob, RK4(), dt = dt0, adaptive = false)
+@show sol.destats
+plot!(sol.t,  mapreduce(permutedims, vcat, sol.u),
+      color = :black, linewidth = 2, line = :dash) |> display
+
+GC.gc()
+println("\ndone")
---
+++ validation/ode/double_pendulum/equations.jl
@@ -0,0 +1,52 @@
+
+const g = 9.81
+
+# for RKM
+function dy_dt!(f, y; p, kwargs...)
+    L = p[1]
+    θ1, θ2, pθ1, pθ2 = y
+
+    cos12 = cos(θ1 - θ2)
+    factor = 6.0/(16.0 - 9.0*cos12^2)
+
+    # d/dt(theta_1)
+    f[1] = (2.0*pθ1 - 3.0*pθ2*cos12) * factor
+    # d/dt(theta_2)
+    f[2] = (8.0*pθ2 - 3.0*pθ1*cos12) * factor
+
+    θdot1 = f[1]
+    θdot2 = f[2]
+    θdot1θdot2sin12 = θdot1 * θdot2 * sin(θ1 - θ2)
+
+    # note: normalized canonical momentum by moment of inertia
+    # d/dt(p_theta_1) / (m*L^2)
+    f[3] = -(θdot1θdot2sin12 + 3.0*sin(θ1)*g/L) / 2.0
+    # d/dt(p_theta_2) / (m*L^2)
+    f[4] = (θdot1θdot2sin12 - sin(θ2)*g/L) / 2.0
+    return nothing
+end
+
+# for OrdinaryDiffEq
+function dy_dt!(f, y, p, t)
+    L = p[1]
+    θ1, θ2, pθ1, pθ2 = y
+
+    cos12 = cos(θ1 - θ2)
+    factor = 6.0/(16.0 - 9.0*cos12^2)
+
+    # d/dt(theta_1)
+    f[1] = (2.0*pθ1 - 3.0*pθ2*cos12) * factor
+    # d/dt(theta_2)
+    f[2] = (8.0*pθ2 - 3.0*pθ1*cos12) * factor
+
+    θdot1 = f[1]
+    θdot2 = f[2]
+    θdot1θdot2sin12 = θdot1 * θdot2 * sin(θ1 - θ2)
+
+    # note: normalized canonical momentum by moment of inertia
+    # d/dt(p_theta_1) / (m*L^2)
+    f[3] = -(θdot1θdot2sin12 + 3.0*sin(θ1)*g/L) / 2.0
+    # d/dt(p_theta_2) / (m*L^2)
+    f[4] = (θdot1θdot2sin12 - sin(θ2)*g/L) / 2.0
+    return nothing
+end
