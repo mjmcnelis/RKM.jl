a26d28b9a564aa6b058c3dd7cd07044376fd3c7f
Michael <Michael@Michael-McNeliss-MacBook-Pro.local>
2023-10-01 15:38:16 -0600

    add option to plot time step:

--- src/plots.jl
+++ src/plots.jl
@@ -6,7 +6,7 @@ Plots the ODE solution `y(t)` from `sol` with the function `plot` (usually `Plot
 Note: pass `plot` as a function to reduce RKM's precompilation time.
 """
 function plot_ode(sol::Solution, method::ODEMethod, plot::Function;
-                  logx = false, logy = false)
+                  logx = false, logy = false, show_time_step = false)
 
     # TODO: put some more functionality (panel?)
     y, t = get_solution(sol)
@@ -22,12 +22,13 @@ function plot_ode(sol::Solution, method::ODEMethod, plot::Function;
                xlabel = "t", xguidefontsize = 14, xtickfontsize = 12,
                x_args..., y_args...)
 
-    # dt = t[2:end] .- t[1:end-1]
-    # plot(t[1:end-1], dt; size = (900, 600), linewidth = 2,
-    #      label = code_name, legend = :outertopright, legendfontsize = 12,
-    #      ylabel = "Δt", yguidefontsize = 14, ytickfontsize = 12,
-    #      xlabel = "t", xguidefontsize = 14, xtickfontsize = 12,) |> display
-
+    if show_time_step
+        dt = t[2:end] .- t[1:end-1]
+        plot(t[1:end-1], dt; size = (900, 600), linewidth = 2,
+            label = code_name, legend = :outertopright, legendfontsize = 12,
+            ylabel = "Δt", yguidefontsize = 14, ytickfontsize = 12,
+            xlabel = "t", xguidefontsize = 14, xtickfontsize = 12,) |> display
+    end
     return plt
 end
 
