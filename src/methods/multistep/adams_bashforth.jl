cff7370e520fce7ce39cb21cea551e4a914abb6b
Michael <Michael@Michael-McNeliss-MacBook-Pro.local>
2023-12-02 17:04:06 -0700

    added b_pred to multistep struct, changed table from Vector to Matrix

--- src/methods/multistep/adams_bashforth.jl
+++ src/methods/multistep/adams_bashforth.jl
@@ -1,42 +1,42 @@
 
 function AdamsBashforth1(; precision::Type{T} = Float64) where T <: AbstractFloat
-    table = [1, 1]
+    table = [1 1]
     table = table .|> precision
     return LinearMultistep(; name = :Adams_Bashforth_1, table)
 end
 
 function AdamsBashforth2(; precision::Type{T} = Float64) where T <: AbstractFloat
-    table = [1, 3//2, -1//2]
+    table = [1 3//2 -1//2]
     table = table .|> precision
     return LinearMultistep(; name = :Adams_Bashforth_2, table)
 end
 
 function AdamsBashforth3(; precision::Type{T} = Float64) where T <: AbstractFloat
-    table = [1, 23//12, -16//12, 5//12]
+    table = [1 23//12 -16//12 5//12]
     table = table .|> precision
     return LinearMultistep(; name = :Adams_Bashforth_3, table)
 end
 
 function AdamsBashforth4(; precision::Type{T} = Float64) where T <: AbstractFloat
-    table = [1, 55//24, -59//24, 37//24, -9//24]
+    table = [1 55//24 -59//24 37//24 -9//24]
     table = table .|> precision
     return LinearMultistep(; name = :Adams_Bashforth_4, table)
 end
 
 function AdamsBashforth5(; precision::Type{T} = Float64) where T <: AbstractFloat
-    table = [1, 1901//720, -2774//720, 2616//720, -1274//720, 251//720]
+    table = [1 1901//720 -2774//720 2616//720 -1274//720 251//720]
     table = table .|> precision
     return LinearMultistep(; name = :Adams_Bashforth_5, table)
 end
 
 function AdamsBashforth6(; precision::Type{T} = Float64) where T <: AbstractFloat
-    table = [1, 4277//1440, -7923//1440, 9982//1440, -7298//1440, 2877//1440, -475//1440]
+    table = [1 4277//1440 -7923//1440 9982//1440 -7298//1440 2877//1440 -475//1440]
     table = table .|> precision
     return LinearMultistep(; name = :Adams_Bashforth_6, table)
 end
 
 function AdamsBashforth8(; precision::Type{T} = Float64) where T <: AbstractFloat
-    table = [1, 434241//120960, -1152169//120960, 2183877//120960, -2664477//120960, 2102243//120960, -1041723//120960, 295767//120960, -36799//120960]
+    table = [1 434241//120960 -1152169//120960 2183877//120960 -2664477//120960 2102243//120960 -1041723//120960 295767//120960 -36799//120960]
     table = table .|> precision
     return LinearMultistep(; name = :Adams_Bashforth_8, table)
 end
--- src/methods/multistep/linear_multistep.jl
+++ src/methods/multistep/linear_multistep.jl
@@ -2,25 +2,29 @@
 struct LinearMultistep{T, S, P} <: ODEMethod where {T <: AbstractFloat, S, P}
     name::Symbol
     b::SVector{S,T}
+    b_pred::SVector{S,T}
     stages::Int64
     order::SVector{P,T}
     iteration::Iteration
     code_name::String
 end
 
-function LinearMultistep(; name::Symbol, table::Vector{T}) where T <: AbstractFloat
+function LinearMultistep(; name::Symbol, table::Matrix{T}) where T <: AbstractFloat
     order     = order_prop(name, T)
     code_name = make_code_name(name)
 
-    ncol, = size(table)
+    nrow, ncol = size(table)
+    @assert 1 <= nrow <= 2 && ncol >= 2
+
     stages = ncol - 1
 
-    b = table[2:ncol] |> SVector{stages}
+    b = table[1, 2:ncol] |> SVector{stages}
+    b_pred = table[max(1,nrow), 2:ncol] |> SVector{stages}
 
     # TMP
     iteration = Explicit()
 
-    return LinearMultistep(name, b, stages, order, iteration, code_name)
+    return LinearMultistep(name, b, b_pred, stages, order, iteration, code_name)
 end
 
 function Base.show(io::IO, LM::LinearMultistep)
