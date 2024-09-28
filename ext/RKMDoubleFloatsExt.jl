122d6bef220231ca241ffaf8b0a0301a10f52cc7
Michael <mjmcnelis@gmail.com>
2024-09-28 11:50:36 -0600

    reorg double float ext

--- ext/RKMDoubleFloatsExt.jl
+++ ext/RKMDoubleFloatsExt.jl
@@ -4,7 +4,7 @@ import RKM: RKM, RKM_root
 import DoubleFloats: IEEEFloat, DoubleFloat
 
 function __init__()
-    include("$RKM_root/src/tmp/ext/double_float.jl")
+    include("$RKM_root/src/tmp/double_float/ext/norm.jl")
 end
 
 end
