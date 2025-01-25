d3bbe0d1a78aeb03684a8c21271365292709f249
Michael <mjmcnelis@gmail.com>
2025-01-24 19:02:42 -0700

    added docstring for nansafe_jacobian

--- src/implicit/jacobian.jl
+++ src/implicit/jacobian.jl
@@ -44,12 +44,31 @@ function evaluate_system_jacobian!(jacobian_method::FiniteJacobian,
     return nothing
 end
 
+"""
+    nansafe_jacobian(y0::Vector{T}, t0::T1, dy_dt!::Function,
+                     p::Vector{Float64} = Float64[]; abstract_params = nothing,
+                     chunk_size::Int64 = 12) where {T <: AbstractFloat,
+                                                    T1 <: AbstractFloat}
+
+Estimates the sparsity pattern of the system Jacobian J = df/dy via forward-mode
+auto-differentiation. The Jacobian is evaluated at y = NaN, which only works if
+NANSAFE_MODE_ENABLED = true in ForwardDiff.jl. Reducing the chunk size can help
+remove excess matrix elements in the sparsity pattern. For more details
+see https://juliadiff.org/ForwardDiff.jl/stable/user/advanced/.
+
+Note: some nonzero matrix elements may be missed if the user function dy_dt!
+      handles NaN values in certain edge cases (e.g. if isnan(x) x = 0 end).
+
+Required parameters: `y0`, `t0`, `dy_dt!`
+
+Optional parameters: `p`, `abstract_params`, `chunk_size`
+"""
 function nansafe_jacobian(y0::Vector{T}, t0::T1, dy_dt!::Function,
-                          p::Vector{Float64} = Float64[]; chunk_size::Int64 = 12,
-                          abstract_params = nothing) where {T <: AbstractFloat,
-                                                            T1 <: AbstractFloat}
+                          p::Vector{Float64} = Float64[]; abstract_params = nothing,
+                          chunk_size::Int64 = 12) where {T <: AbstractFloat,
+                                                         T1 <: AbstractFloat}
     if !NANSAFE_MODE_ENABLED
-        @warn "nansafe_mode is false, sparsity pattern will likely be dense."
+        @warn "nansafe mode is not enabled, sparsity pattern will likely be dense."
         println("""\nRun the following commands in your base (project) environment and
                 restart the Julia REPL:
 
@@ -62,7 +81,7 @@ function nansafe_jacobian(y0::Vector{T}, t0::T1, dy_dt!::Function,
     ny = length(y0)
     J = zeros(ny, ny)
 
-    y = NaN.*y0
+    y = [NaN for i in 1:ny]
     f = similar(y0)
     ode_wrap! = ODEWrapperState([t0], p, abstract_params, dy_dt!)
     cache = JacobianConfig(ode_wrap!, f, y, Chunk(chunk_size))
